c71b9038bd18510e295f5802b0f72f4611957acc
lavaxcii <lavaxcii@gmail.com>
2023-07-05 13:59:54 +0200

    Correct edit window input error.

--- src/components/EditWindow.js
+++ src/components/EditWindow.js
@@ -5,7 +5,7 @@ export default class EditWindow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      itemForModNewContent: {text: '', id: ''}
+      itemForModNewContent: {text: ' ', id: ''}
     };
     this.setItemForModNewContent = this.setItemForModNewContent.bind(this);
   }
@@ -23,7 +23,6 @@ export default class EditWindow extends Component {
 
     return (
       <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.50, ease: "easeOut" }} className='absolute flex flex-col content-center items-center justify-center h-full w-full z-10 left-0 top-0'>
-        {itemForModNewContent.text === '' ? setItemForModNewContent(dataArrayToCarry[itemForModValue].text, dataArrayToCarry[itemForModValue].id) : null}
         <form className='flex flex-col items-center bg-gradient-to-r from-blue-300 to-green-400 border-solid border-2 border-black text-xl w-full' action="" onSubmit={function (e) {
           e.target.checkValidity();
           e.preventDefault();
@@ -33,6 +32,7 @@ export default class EditWindow extends Component {
           <label>Modify Item #{itemForModValue}</label>
           <input className='text-center text-ellipsis mb-2 w-3/4 text-xl border-outset border-2 border-black' onChange={e => setItemForModNewContent(e.target.value, dataArrayToCarry[itemForModValue].id)} value={itemForModNewContent.text} type="text" minLength={1} maxLength={13} pattern={'^[A-Za-z0-9]+$'} required />
           <button className='flex text-black hover:bg-gradient-to-r from-green-300 to-blue-400 border-black border-solid border-2 p-1 m-5 text-lg bg-slate-100 rounded-full'>Confirm change</button>
+          {itemForModNewContent.text === ' ' ? setItemForModNewContent(dataArrayToCarry[itemForModValue].text, dataArrayToCarry[itemForModValue].id) : null}
         </form>
       </m.div>
     )
